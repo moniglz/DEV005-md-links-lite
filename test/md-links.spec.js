d90bd880b16832cba24844fc80e517c5f0c630e3
moniglz <monie.gop@gmail.com>
2023-06-10 22:22:59 -0600

    UPDATE testing.spec.js, adding mocks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,98 @@
-const mdLinks = require('./mdLinks');
+const mdLinks = require('../mdLinks');
+const validateRoute = require('../validate');
+const readingFile = require('../file_reader');
+const validateLinks = require('../link_validation');
+
+jest.mock('../validate');
+jest.mock('../file_reader');
+jest.mock('../link_validation');
 
 describe('mdLinks', () => {
-  it('should...', () => {
-    console.log('FIX ME!');
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should reject with an error if the file path doesn\'t exist', () => {
+    const path = 'nonexistent/file.md';
+    const options = {};
+
+    validateRoute.mockReturnValue(false);
+
+    return expect(mdLinks(path, options)).rejects.toThrow("The file path doesn't exist.");
+  });
+
+  test('should return an array of links if `validate` option is not provided', () => {
+    const path = 'existing/file.md';
+    const options = {};
+
+    const expectedResult = [
+      {
+        href: 'https://example.com',
+        text: 'Example',
+        file: 'existing/file.md',
+      },
+      // Add more expected link objects here if necessary
+    ];
+
+    const mockResult = {
+      links: [
+        {
+          href: 'https://example.com',
+          text: 'Example',
+          file: 'existing/file.md',
+        },
+        // Add more mock link objects here if necessary
+      ],
+    };
+
+    validateRoute.mockReturnValue(true);
+    readingFile.mockResolvedValue(mockResult);
+
+    return expect(mdLinks(path, options)).resolves.toEqual(expectedResult);
+  });
+
+  test('should return an array of validated links if `validate` option is true', () => {
+    const path = 'existing/file.md';
+    const options = {
+      validate: true,
+    };
+
+    const expectedResult = [
+      {
+        href: 'https://example.com',
+        text: 'Example',
+        file: 'existing/file.md',
+        status: 200,
+        ok: true,
+      },
+      // Add more expected validated link objects here if necessary
+    ];
+
+    const mockResult = {
+      links: [
+        {
+          href: 'https://example.com',
+          text: 'Example',
+          file: 'existing/file.md',
+        },
+        // Add more mock link objects here if necessary
+      ],
+    };
+
+    validateRoute.mockReturnValue(true);
+    readingFile.mockResolvedValue(mockResult);
+    validateLinks.mockResolvedValue(expectedResult);
+
+    return expect(mdLinks(path, options)).resolves.toEqual(expectedResult);
   });
 });
+
+
+
+
+
+// describe('mdLinks', () => {
+//   it('should return an array', () => {
+//     console.log('FIX ME!');
+//   });
+// });
--- test/testing.spec.js
+++ test/testing.spec.js
@@ -1,31 +1,37 @@
 const path = require('path');
+const fs = {
+  existsSync: jest.fn(),
+  readFileSync: jest.fn(),
+  readFile: jest.fn(),
+};
+
 const validateRoute = require('../validate');
 const readingFile = require('../file_reader');
-const fs = require('fs');
+const extractLinks = require('../link_extraction');
 
-//Test de validate.js
+
+// Test de validate.js
+jest.mock('fs', () => ({
+  existsSync: jest.fn(),
+}));
+
 describe('validateRoute', () => {
   test('its a function', () => {
     expect(typeof validateRoute).toBe('function');
   });
 
-  test('should return an absolute path if route exists and is absolute', () => {
-    const route = 'C:/Users/Moni/Documents/Laboratoria/Proyectos/MDLinks/DEV005-md-links-lite/thumb.png';
-    const expectedResult = path.normalize(route); // Normaliza la ruta (`\` to `/`)
-    const result = validateRoute(route);
-    expect(result).toBe(expectedResult);
-  });
-
-  it('should return a resolved path if route exists and is relative', () => {
+  it('should return an absolute path if route exists and is absolute', () => {
+    fs.existsSync.mockReturnValue(true);
     const route = './thumb.png';
     const expectedResult = path.resolve(route);
     const result = validateRoute(route);
+    
     expect(result).toBe(expectedResult);
   });
 
-  it('should return undefined if route doesn\'t exists', () => {
-    const route = './thumb.txt';
-    const result = validateRoute(route);
+  it(`should return undefined if route doesn't exists`, () => {
+    fs.existsSync.mockReturnValue(false);
+    const result = validateRoute('./thumb.jpeg');
     expect(result).toBeUndefined();
   });
 });
@@ -35,30 +41,74 @@ describe('readingFile', () => {
   test('its a function', () => {
     expect(typeof readingFile).toBe('function');
   });
-
+  
   it('should return extracted links for a .md file', async () => {
-    const route = './example.md';
+    const route = './otherSample.md';
 
-    const expectedLinks = [
-      {
-        file: './example.md',
-        href: 'https://curriculum.laboratoria.la/es/topics/javascript/02-flow-control/03-functions',
-        text: 'Funciones (control de flujo)',
-      },
-      {
-        file: './example.md',
-        href: 'https://jestjs.io/docs/es-ES/getting-started',
-        text: 'Empezando con Jest - Documentación oficial',
-      },
-    ];
+    fs.existsSync.mockReturnValue(true);
+    const data = fs.readFileSync(route, 'utf8');
+    const expectedResult = {
+      extension: '.md',
+      links: [
+        {
+          href: 'https://youtu.be/LlOa6KiiAw0',
+          text: 'Pájaros - Porter',
+          file: path.resolve(route),
+        },
+        {
+          href: 'https://agilemanifesto.org/iso/es/manifesto.html',
+          text: 'Manifiesto Ágil',
+          file: path.resolve(route),
+        },
+        {
+          href: 'https://flexboxfroggy.com/#es',
+          text: 'Game flexbox froggy',
+          file: path.resolve(route),
+        },
+        {
+          href: 'https://www.happyhueees.com',
+          text: 'Link trampa',
+          file: path.resolve(route),
+        },
+        {
+          href: 'https://www.happyhues.co/',
+          text: 'Happy Hues',
+          file: path.resolve(route),
+        }
+      ].map(link => {
+        return {
+          ...link,
+          file: path.normalize(link.file)
+        };
+      })
+    };
 
     const result = await readingFile(route);
-    expect(result).toBe(expectedLinks);
+    expect(result).toEqual(expectedResult)
   });
 
-  it('should reject the promise for non .md file', () => {
-    const route = './example.txt';
+  it('should return an empty array of links if there are no links in the file', async () => {
+    const route = './emptySample.md';
+    const data = 'This is a sample file without links';
+    
+    fs.readFile.mockImplementation((path, options, callback) => {
+      callback(null, data);
+    });
+  
+    extractLinks.mockReturnValue([]);
+  
+    const result = await readingFile(route);  
+    expect(result.extension).toBe('.md');
+    expect(result.links).toEqual([]);
+  });
 
-    return expect(readingFile(route)).rejects.toThrowError(`It isn't a .md file, it's a: .txt file`);
+  it('should reject if the file extension is not a .md', async () => {
+    const route = './file.txt';
+
+    fs.existsSync.mockReturnValue(true);
+    const error = new Error(`It isn't a .md file, it's a: .txt file`);
+    readingFile.mockRejectedValue(error);
+
+    await expect(readingFile(route)).rejects.toThrowError(error);
   });
 });
